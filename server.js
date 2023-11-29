ce6009fc5ba114930dc68c4dc9714f9844f99435
Jungsu <jungsukim2609@gmail.com>
2023-11-28 23:10:59 -0700

    Deployment

--- server.js
+++ server.js
@@ -8,15 +8,12 @@ const cors = require('cors');
 const { ReadableStreamDefaultController } = require('stream/web');
 const path = require('path');
 
-const _dirname = path.dirname("");
-const buildPath = path.join(_dirname, "/build");
+const buildPath = path.join(__dirname, "build");
 
 app.use(express.static(buildPath));
 
 app.get("/*", function(req, res) {
-  console.log(_dirname);
-  res.sendFile(
-    path.join(_dirname, "/build/index.html"),
+  res.sendFile(path.join(buildPath, 'index.html'),
     function(err) {
       if(err) {
         res.status(500).send(err);
