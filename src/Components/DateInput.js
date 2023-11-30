ba7b173b2483131e44b8f5096e97a8e0cc68a9b9
doowoncho <84430881+doowoncho@users.noreply.github.com>
2023-11-30 16:15:22 -0700

    date input placeholders

--- src/Components/DateInput.js
+++ src/Components/DateInput.js
@@ -1,15 +1,24 @@
-export default function DateInput({ date, index, handleDateChange, deleteDate, editMode }) {
+import { useState } from "react";
+
+export default function DateInput({ date, index, handleDateChange, deleteDate }) {
+  const [showAdditionalInput, setShowAdditionalInput] = useState(false);
+
+  const handleCheckboxChange = () => {
+    setShowAdditionalInput(!showAdditionalInput);
+  };
+
     return (
       <div className="d-flex align-items-center">
-        <label>Date</label>
+        {showAdditionalInput ? <label>Start Date</label> : <label>Date</label>}
         <input
           type="date"
           className="form-control mx-2"
           value={date.date}
-          style={{ width: '200px' }}
+          style={{ width: '150px' }}
           onChange={(e) => handleDateChange(index, 'date', e.target.value)}
           required
-        />
+          />
+
         <label>Start Time</label>
         <input
           type="time"
@@ -19,6 +28,21 @@ export default function DateInput({ date, index, handleDateChange, deleteDate, e
           onChange={(e) => handleDateChange(index, 'startTime', e.target.value)}
           required
         />
+
+      {showAdditionalInput && (
+        <>
+        <label>End Date</label>
+        <input
+          type="date"
+          className="form-control mx-2"
+          value={date.date}
+          style={{ width: '200px' }}
+          onChange={(e) => handleDateChange(index, 'date', e.target.value)}
+          required
+          />
+        </>
+      )}
+
         <label>End Time</label>
         <input
           type="time"
@@ -28,11 +52,16 @@ export default function DateInput({ date, index, handleDateChange, deleteDate, e
           onChange={(e) => handleDateChange(index, 'endTime', e.target.value)}
           required
         />
-       
-      <input className="form-check-input mx-2" type="checkbox" value=""/>
-      <label className="form-check-label" for="flexCheckDefault"> 24/7 Job</label>    
+      <input className="form-check-input mx-2" type="checkbox" value="" onChange={handleCheckboxChange}/>
+      <label className="form-check-label" for="flexCheckDefault"> 24/7</label>  
+      
+      {showAdditionalInput && (
+        <>
       <input className="form-check-input mx-2" type="checkbox" value=""/>
       <label className="form-check-label" for="flexCheckDefault"> Exclude Weekends</label>
+      </>
+      )}
+
       <input className="form-check-input mx-2" type="checkbox" value=""/>
       <label className="form-check-label" for="flexCheckDefault"> NPAT</label>
 
