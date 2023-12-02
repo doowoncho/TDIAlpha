11f80619b87ab58b036e9b16c63453dfc08786a9
Jungsu <jungsukim2609@gmail.com>
2023-12-01 17:37:14 -0700

    Maybe?

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const buildPath = path.join(__dirname, "build");
 
 app.use(express.static(buildPath));
 
-app.get("/*", function(req, res) {
+app.get("/", function(req, res) {
   res.sendFile(path.join(buildPath, 'index.html'),
     function(err) {
       if(err) {
