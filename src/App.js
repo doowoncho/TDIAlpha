2f00ad765e784d1ef7679bd4c5341f4439c27fde
doowoncho <84430881+doowoncho@users.noreply.github.com>
2023-12-20 02:27:04 -0700

    moved the auth

--- src/App.js
+++ src/App.js
@@ -1,68 +1,66 @@
+import { Route, Routes, Navigate } from "react-router-dom";
+import HomePage from "./Page/HomePage";
+import TasksTablePage from "./Page/TasksTablePage";
+import FormPage from "./Page/FormPage";
+import TaskDetails from "./Page/TaskDetails";
+import LoginPage from "./Page/LoginPage";
+import TimePage from "./Page/TimePage";
+import ToDoPage from "./Page/ToDoPage";
+import InvoicePage from "./Page/InvoicePage";
+import CompletedPage from "./Page/CompletedPage";
+import JobsTable from "./Page/JobsTablePage";
 
-// import { Route, Routes, Navigate } from "react-router-dom";
-// import HomePage from "./Page/HomePage";
-// import TasksTablePage from "./Page/TasksTablePage";
-// import FormPage from "./Page/FormPage";
-// import TaskDetails from "./Page/TaskDetails";
-// import LoginPage from "./Page/LoginPage";
-// import TimePage from "./Page/TimePage";
-// import ToDoPage from "./Page/ToDoPage";
-// import InvoicePage from "./Page/InvoicePage";
-// import CompletedPage from "./Page/CompletedPage";
-// import JobsTable from "./Page/JobsTablePage";
 
+const App = () => {
+  const isUserLoggedIn = window.sessionStorage.getItem("user");
 
-// const App = () => {
-//   const isUserLoggedIn = window.sessionStorage.getItem("user");
-
-//   return (
-//     <Routes>
-//       {isUserLoggedIn ? (
-//         <>
-//           <Route path="/" element={<JobsTable />} />
-//           <Route path="/taskspage/:id" element={<TasksTablePage />} />
-//           <Route path="/taskdetails/:id" element={<TaskDetails />} />
-//           <Route path="/todo" element={<ToDoPage />} />
-//           <Route path="/form" element={<FormPage />} />
-//           <Route path="/time" element={<TimePage />} />
-//           <Route path="/invoices" element={<InvoicePage />} />
-//           <Route path="/completed" element={<CompletedPage />} />
-//           <Route path="/jobstable" element={<JobsTable />} />
-//         </>
-//       ) : (
-//         <>
-//         <Route path="/" element={<LoginPage />} />
-//         <Route path="/taskspage/:id" element={<LoginPage />} />
-//         <Route path="/taskdetails/:id" element={<LoginPage />} />
-//         <Route path="/todo" element={<LoginPage />} />
-//         <Route path="/form" element={<LoginPage />} />
-//         <Route path="/time" element={<LoginPage />} /> 
-//         <Route path="/invoices" element={<LoginPage />} />
-//         <Route path="/completed" element={<LoginPage />} />
-//         <Route path="/jobstable" element={<LoginPage />} />
-//         </>
-//       )}
-//       <Route path="/login" element={<LoginPage />} />
-//     </Routes>
-//   );
-// }
-
-// export default App;
-
-import { Amplify } from 'aws-amplify';
-
-import { withAuthenticator } from '@aws-amplify/ui-react';
-import '@aws-amplify/ui-react/styles.css';
-import config from './amplifyconfiguration.json';
-Amplify.configure(config);
-
-function App({ signOut, user }) {
   return (
-    <>
-      <h1>Hello {user.username}</h1>
-      <button onClick={signOut}>Sign out</button>
-    </>
+    <Routes>
+      {isUserLoggedIn ? (
+        <>
+          <Route path="/" element={<JobsTable />} />
+          <Route path="/taskspage/:id" element={<TasksTablePage />} />
+          <Route path="/taskdetails/:id" element={<TaskDetails />} />
+          <Route path="/todo" element={<ToDoPage />} />
+          <Route path="/form" element={<FormPage />} />
+          <Route path="/time" element={<TimePage />} />
+          <Route path="/invoices" element={<InvoicePage />} />
+          <Route path="/completed" element={<CompletedPage />} />
+          <Route path="/jobstable" element={<JobsTable />} />
+        </>
+      ) : (
+        <>
+        <Route path="/" element={<LoginPage />} />
+        <Route path="/taskspage/:id" element={<LoginPage />} />
+        <Route path="/taskdetails/:id" element={<LoginPage />} />
+        <Route path="/todo" element={<LoginPage />} />
+        <Route path="/form" element={<LoginPage />} />
+        <Route path="/time" element={<LoginPage />} /> 
+        <Route path="/invoices" element={<LoginPage />} />
+        <Route path="/completed" element={<LoginPage />} />
+        <Route path="/jobstable" element={<LoginPage />} />
+        </>
+      )}
+      <Route path="/login" element={<LoginPage />} />
+    </Routes>
   );
 }
 
-export default withAuthenticator(App);
+export default App;
+// import { Amplify } from 'aws-amplify';
+
+// import { withAuthenticator } from '@aws-amplify/ui-react';
+// import '@aws-amplify/ui-react/styles.css';
+// import config from './amplifyconfiguration.json';
+// Amplify.configure(config);
+
+// function App({ signOut, user }) {
+//   return (
+//     <>
+//       <h1>Hello {user.username}</h1>
+//       <button onClick={signOut}>Sign out</button>
+//     </>
+//   );
+// }
+
+// export default withAuthenticator(App);
--- src/index.js
+++ src/index.js
@@ -6,6 +6,9 @@ import { BrowserRouter } from 'react-router-dom';
 
 import App from './App';
 import MyNavBar from './components/MyNavBar';
+import { Amplify } from 'aws-amplify';
+import config from './amplifyconfiguration.json';
+Amplify.configure(config);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
