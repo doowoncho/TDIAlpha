bdaf5e8b8981cf6eb27832380e5c23a87036a9ce
doowoncho <84430881+doowoncho@users.noreply.github.com>
2023-12-20 02:32:38 -0700

    auth

--- src/index.js
+++ src/index.js
@@ -6,9 +6,6 @@ import { BrowserRouter } from 'react-router-dom';
 
 import App from './App';
 import MyNavBar from './components/MyNavBar';
-import { Amplify } from 'aws-amplify';
-import config from './amplifyconfiguration.json';
-Amplify.configure(config);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
