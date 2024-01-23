88b641d07593bf9e60113e8999373db289c2cef6
doowoncho <84430881+doowoncho@users.noreply.github.com>
2024-01-23 14:52:53 -0700

    cors delete

--- server.js
+++ server.js
@@ -37,11 +37,6 @@ app.get("/", function(req, res) {
   )
 })
 
-app.use(cors({
-    // // origin: 'https://main.d3uj1gkliipo6a.amplifyapp.com',
-    //    origin: 'http://localhost:3001',
-  }));
-
 //api endpoints to be called in the code to make calls in the database
 
 app.get('/api/tasks', async (req, res) => {
