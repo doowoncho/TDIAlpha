c2c55fbf9757c60f42e6f6fe2aff05f5f649ec79
doowoncho <84430881+doowoncho@users.noreply.github.com>
2024-03-16 12:39:55 -0600

    row colors working

--- src/Components/Table.js
+++ src/Components/Table.js
@@ -3,6 +3,8 @@ import Box from '@mui/material/Box';
 import { DataGrid, GridToolbar } from '@mui/x-data-grid';
 import Snackbar from '@mui/material/Snackbar'; // Import Snackbar from @mui/material
 
+import '../Styles/Rows.css'
+
 export default function Table({ data, columns, handleUpdate }) {
   const [snackbarMessage, setSnackbarMessage] = useState('');
   const [snackbarOpen, setSnackbarOpen] = useState(false);
@@ -16,6 +18,24 @@ export default function Table({ data, columns, handleUpdate }) {
         setSnackbarOpen(true);
       }
       return updatedRow
+  };
+
+  const getRowClassName = (params) => {
+    const isComplete = params.row.completed
+    const rowType = params.row.type; // Change 'status' to the field you want to base the color on
+
+    if(isComplete){
+      return 'completed-row'; 
+    }
+
+    switch (rowType) {
+      case 'place':
+        return 'place-row'; // Define CSS class for pending rows
+      case 'npat':
+        return 'npat-row'; // Define CSS class for completed rows
+      default:
+        return ''; // Default class for other rows
+    }
   };
 
   const handleProcessRowUpdateError = (error) => {
@@ -38,6 +58,7 @@ export default function Table({ data, columns, handleUpdate }) {
     <div className='container my-3'>
       <Box sx={{ height: '500px', width: '100%' }}>
         <DataGrid
+          className="custom-datagrid"
           slots={{ toolbar: GridToolbar }}
           rows={data}
           columns={columns}
@@ -47,6 +68,7 @@ export default function Table({ data, columns, handleUpdate }) {
           // disableColumnFilter
           density="comfortable"
           editMode="cell" // Set editMode to "row" to prevent cells from popping out after editing
+          getRowClassName={getRowClassName} //
         />
       </Box>
       <Snackbar open={snackbarOpen} onClose={handleCloseSnackbar} message={snackbarMessage} />
